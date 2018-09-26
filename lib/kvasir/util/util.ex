6b51174b87e1857799d7767f2255e782c12bd148
Ian Luites <IanLuites@users.noreply.github.com>
2018-09-26 08:20:50 +0200

    Allow for `.` in event name.

--- lib/kvasir/util/util.ex
+++ lib/kvasir/util/util.ex
@@ -5,6 +5,7 @@ defmodule Kvasir.Util do
   @spec name(any) :: String.t()
   def name(name) when is_binary(name), do: name
   def name(name) when is_atom(name), do: to_string(name)
+  def name({{:., _, names}, _, _}), do: names |> Enum.map(&name/1) |> Enum.join(".")
   def name({name, _, nil}) when is_atom(name), do: to_string(name)
 
   @doc false
