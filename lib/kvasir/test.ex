1ae441bec7b1dca01d6429da4487adb39721cc61
Ian Luites <IanLuites@users.noreply.github.com>
2018-09-26 08:22:23 +0200

    Add event test helper.

---
+++ lib/kvasir/test.ex
@@ -0,0 +1,24 @@
+defmodule Kvasir.Test do
+  alias Kvasir.Event.Encodings.Raw
+  @unix ~N[1970-01-01 00:00:00]
+
+  def event(event, payload, opts \\ []) do
+    Raw.decode(
+      %{
+        payload: payload,
+        meta: %{
+          topic: opts[:topic] || "test",
+          partition: opts[:partition] || 0,
+          offset: opts[:offset] || %{0 => 1},
+          key: opts[:instance],
+          ts_type: "woef",
+          ts: NaiveDateTime.diff(NaiveDateTime.utc_now(), @unix, :millisecond),
+          headers: [],
+          command: opts[:command]
+        },
+        type: event.__event__(:type)
+      },
+      opts
+    )
+  end
+end
