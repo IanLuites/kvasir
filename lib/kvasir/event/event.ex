b568079ca7d273521a2c32e25d9d360db4bea43c
Ian Luites <IanLuites@users.noreply.github.com>
2019-12-30 12:49:19 +0100

    Add event details (hex, hexdocs, source) to events.

--- lib/kvasir/event/event.ex
+++ lib/kvasir/event/event.ex
@@ -116,6 +116,35 @@ defmodule Kvasir.Event do
   end
 
   defmacro event(type, do: block) do
+    config = Mix.Project.config()
+    app = Keyword.get(config, :app)
+    version = Keyword.get(config, :version)
+    docs = Keyword.get(config, :docs, [])
+    package = Keyword.get(config, :package, [])
+    dep_depth = config |> Keyword.get(:deps_path) |> Path.split() |> Enum.count()
+    path = __CALLER__.file |> Path.split() |> Enum.slice((dep_depth + 1)..-1) |> Path.join()
+
+    {hex, hexdocs} =
+      case {Keyword.get(package, :name), Keyword.get(package, :organization)} do
+        {nil, _} ->
+          nil
+
+        {app, nil} ->
+          {"https://hex.pm/packages/#{app}/#{version}",
+           "https://hexdocs.pm/#{app}/#{version}/#{inspect(__CALLER__.module)}.html"}
+
+        {app, org} ->
+          {"https://hex.pm/packages/#{org}/#{app}/#{version}",
+           "https://#{org}.hexdocs.pm/#{app}/#{version}/#{inspect(__CALLER__.module)}.html"}
+      end
+
+    source =
+      case {Keyword.get(docs, :source_url), Keyword.get(docs, :source_ref)} do
+        {nil, _} -> nil
+        {url, nil} -> url <> "/src/#{path}"
+        {url, ref} -> url <> "/src/#{ref}/#{path}"
+      end
+
     quote do
       Module.register_attribute(__MODULE__, :fields, accumulate: true)
 
@@ -150,6 +179,10 @@ defmodule Kvasir.Event do
       def __event__(:doc), do: @moduledoc
       def __event__(:version), do: @current_version
       def __event__(:history), do: @version_history
+      def __event__(:app), do: {unquote(app), unquote(version)}
+      def __event__(:hex), do: unquote(hex)
+      def __event__(:hexdocs), do: unquote(hexdocs)
+      def __event__(:source), do: unquote(source)
 
       @field_type Map.new(@fields, fn {k, v, _} -> {k, v} end)
       @doc false
