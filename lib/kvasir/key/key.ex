97b4085c5c3d1afadfa236230a1b3187af77a8ae
Ian Luites <IanLuites@users.noreply.github.com>
2020-01-09 01:22:25 +0100

    Document keys.

--- lib/kvasir/key/key.ex
+++ lib/kvasir/key/key.ex
@@ -20,6 +20,35 @@ defmodule Kvasir.Key do
               {:ok, term} | :obfuscate | {:error, atom}
 
   defmacro __using__(opts \\ []) do
+    config = Mix.Project.config()
+    app = Keyword.get(config, :app)
+    version = Keyword.get(config, :version)
+    docs = Keyword.get(config, :docs, [])
+    package = Keyword.get(config, :package, [])
+    dep_depth = config |> Keyword.get(:deps_path) |> Path.split() |> Enum.count()
+    path = __CALLER__.file |> Path.split() |> Enum.slice((dep_depth + 1)..-1) |> Path.join()
+
+    {hex, hexdocs} =
+      case {Keyword.get(package, :name), Keyword.get(package, :organization)} do
+        {nil, _} ->
+          nil
+
+        {app, nil} ->
+          {"https://hex.pm/packages/#{app}/#{version}",
+           "https://hexdocs.pm/#{app}/#{version}/#{inspect(__CALLER__.module)}.html"}
+
+        {app, org} ->
+          {"https://hex.pm/packages/#{org}/#{app}/#{version}",
+           "https://#{org}.hexdocs.pm/#{app}/#{version}/#{inspect(__CALLER__.module)}.html"}
+      end
+
+    source =
+      case {Keyword.get(docs, :source_url), Keyword.get(docs, :source_ref)} do
+        {nil, _} -> nil
+        {url, nil} -> url <> "/src/#{path}"
+        {url, ref} -> url <> "/src/#{ref}/#{path}"
+      end
+
     name =
       if n = opts[:name],
         do: n,
@@ -75,6 +104,11 @@ defmodule Kvasir.Key do
       @doc false
       @spec __key__(atom) :: term
       def __key__(:name), do: unquote(name)
+      def __key__(:app), do: {unquote(app), unquote(version)}
+      def __key__(:doc), do: @shared_doc
+      def __key__(:hex), do: unquote(hex)
+      def __key__(:hexdocs), do: unquote(hexdocs)
+      def __key__(:source), do: unquote(source)
 
       unquote(base)
       defoverridable parse: 2, dump: 2, obfuscate: 2
