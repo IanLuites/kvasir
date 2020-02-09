c463ed53cbbcd22960942897bd0a2451ff6ee747
Ian Luites <IanLuites@users.noreply.github.com>
2020-02-09 21:17:07 +0100

    Created filted modules.

--- lib/kvasir/event/encoding/topic.ex
+++ lib/kvasir/event/encoding/topic.ex
@@ -6,9 +6,28 @@ defmodule Kvasir.Event.Encoding.Topic do
     |> Enum.join(".")
     |> (&Module.concat(source, &1)).()
     |> generate_module(topic, extra: extra, overwrite: true, events: :all)
+    |> elem(1)
   end
 
-  def generate_module(module, topic, opts \\ []) do
+  def create(source, topic, opts \\ []) do
+    {mod, code} =
+      topic.topic
+      |> String.split(".")
+      |> Enum.map(&Macro.camelize/1)
+      |> Enum.join(".")
+      |> (&Module.concat(source, &1)).()
+      |> generate_module(topic, opts)
+
+    if code do
+      Code.compiler_options(ignore_module_conflict: true)
+      Code.compile_quoted(code)
+      Code.compiler_options(ignore_module_conflict: false)
+    end
+
+    mod
+  end
+
+  defp generate_module(module, topic, opts) do
     {events, mod} =
       case opts[:only] do
         all when all in [nil, :all] ->
@@ -16,12 +35,13 @@ defmodule Kvasir.Event.Encoding.Topic do
 
         es ->
           types = es |> Enum.map(& &1.__event__(:type)) |> Enum.sort()
+          hash = :md5 |> :crypto.hash(types) |> Base.encode16()
 
-          {es, Module.concat(__MODULE__, :md5 |> :crypto.hash(types) |> Base.encode16())}
+          {es, Module.concat(module, "F" <> hash)}
       end
 
     if not Keyword.get(opts, :overwrite, false) and Code.ensure_loaded?(mod) do
-      mod
+      {mod, nil}
     else
       key = topic.key
       decoder = Kvasir.Event.Encoding
@@ -132,130 +152,131 @@ defmodule Kvasir.Event.Encoding.Topic do
             )
         end
 
-      quote do
-        defmodule unquote(mod) do
-          @moduledoc """
-          Event encoding and decoding for the `"#{unquote(topic.topic)}"` topic.
-          """
-          alias Kvasir.Event.Meta
-          alias Kvasir.Type.Serializer, as: S
-          import unquote(decoder), only: [encode: 3, binary_decode: 4, decode: 4]
+      {mod,
+       quote do
+         defmodule unquote(mod) do
+           @moduledoc """
+           Event encoding and decoding for the `"#{unquote(topic.topic)}"` topic.
+           """
+           alias Kvasir.Event.Meta
+           alias Kvasir.Type.Serializer, as: S
+           import unquote(decoder), only: [encode: 3, binary_decode: 4, decode: 4]
 
-          require unquote(compressor)
-          require unquote(encrypter)
+           require unquote(compressor)
+           require unquote(encrypter)
 
-          ### Encoders ###
+           ### Encoders ###
 
-          @doc ~S"""
-          Encode an event to JSON.
+           @doc ~S"""
+           Encode an event to JSON.
 
-          ## Examples
+           ## Examples
 
-          ```elixir
-          iex> encode(⊰Event{...}⊱)
-          {:ok, "{...}"}
-          ```
-          """
-          @spec encode(Kvasir.Event.t()) :: {:ok, binary} | {:error, term}
-          def encode(event)
+           ```elixir
+           iex> encode(⊰Event{...}⊱)
+           {:ok, "{...}"}
+           ```
+           """
+           @spec encode(Kvasir.Event.t()) :: {:ok, binary} | {:error, term}
+           def encode(event)
 
-          def encode(event = %t{}) do
-            with {:ok, data} <- encode(unquote(Macro.escape(topic)), event, []),
-                 {:ok, encoded} <- Jason.encode(data),
-                 {:ok, compressed} <- encrypt(t, encoded),
-                 do: encrypt(t, compressed)
-          end
+           def encode(event = %t{}) do
+             with {:ok, data} <- encode(unquote(Macro.escape(topic)), event, []),
+                  {:ok, encoded} <- Jason.encode(data),
+                  {:ok, compressed} <- encrypt(t, encoded),
+                  do: encrypt(t, compressed)
+           end
 
-          @doc ~S"""
-          Encode an event to binary.
+           @doc ~S"""
+           Encode an event to binary.
 
-          ## Examples
+           ## Examples
 
-          ```elixir
-          iex> bin_encode(⊰Event{...}⊱)
-          {:ok, <<...>>}
-          ```
-          """
-          @spec bin_encode(Kvasir.Event.t()) :: {:ok, binary} | {:error, term}
-          def bin_encode(event)
+           ```elixir
+           iex> bin_encode(⊰Event{...}⊱)
+           {:ok, <<...>>}
+           ```
+           """
+           @spec bin_encode(Kvasir.Event.t()) :: {:ok, binary} | {:error, term}
+           def bin_encode(event)
 
-          def bin_encode(event = %t{__meta__: meta}) do
-            with {:ok, m} <- Meta.encode(meta, unquote(key)),
-                 {:ok, p} <- S.encode(t.__event__(:fields), event),
-                 unpacked <- if(m == %{}, do: p, else: [m, p]),
-                 {:ok, packed} <- Msgpax.pack(unpacked, iodata: false),
-                 {:ok, compressed} <- compress(t, packed),
-                 {:ok, data} <- encrypt(t, compressed) do
-              type = t.__event__(:type)
-              %{major: major, minor: minor, patch: patch} = t.__event__(:version)
+           def bin_encode(event = %t{__meta__: meta}) do
+             with {:ok, m} <- Meta.encode(meta, unquote(key)),
+                  {:ok, p} <- S.encode(t.__event__(:fields), event),
+                  unpacked <- if(m == %{}, do: p, else: [m, p]),
+                  {:ok, packed} <- Msgpax.pack(unpacked, iodata: false),
+                  {:ok, compressed} <- compress(t, packed),
+                  {:ok, data} <- encrypt(t, compressed) do
+               type = t.__event__(:type)
+               %{major: major, minor: minor, patch: patch} = t.__event__(:version)
 
-              {:ok,
-               <<type::binary, 0, major::size(16), minor::size(16), patch::size(16),
-                 data::binary>>}
-            end
-          end
+               {:ok,
+                <<type::binary, 0, major::size(16), minor::size(16), patch::size(16),
+                  data::binary>>}
+             end
+           end
 
-          defp compress(event, data)
-          unquote(compression)
+           defp compress(event, data)
+           unquote(compression)
 
-          defp encrypt(event, data)
-          unquote(encryption)
+           defp encrypt(event, data)
+           unquote(encryption)
 
-          ### Decoders ###
+           ### Decoders ###
 
-          @doc ~S"""
-          Decode a JSON encoded event.
+           @doc ~S"""
+           Decode a JSON encoded event.
 
-          ## Examples
+           ## Examples
 
-          ```elixir
-          iex> decode("{...}")
-          {:ok, ⊰Event{...}⊱}
-          ```
-          """
-          @spec decode(binary) :: {:ok, Kvasir.Event.t()} | {:error, term}
-          def decode(event) do
-            with {:ok, unpacked} <- Jason.decode(event),
-                 {:ok, e} <- event(MapX.get(unpacked, :type)) do
-              decode(e, unquote(key), unpacked, [])
-            end
-          end
+           ```elixir
+           iex> decode("{...}")
+           {:ok, ⊰Event{...}⊱}
+           ```
+           """
+           @spec decode(binary) :: {:ok, Kvasir.Event.t()} | {:error, term}
+           def decode(event) do
+             with {:ok, unpacked} <- Jason.decode(event),
+                  {:ok, e} <- event(MapX.get(unpacked, :type)) do
+               decode(e, unquote(key), unpacked, [])
+             end
+           end
 
-          @doc ~S"""
-          Decode a binary encoded event.
+           @doc ~S"""
+           Decode a binary encoded event.
 
-          ## Examples
+           ## Examples
 
-          ```elixir
-          iex> bin_decode(<<...>>)
-          {:ok, ⊰Event{...}⊱}
-          ```
-          """
-          @spec bin_decode(binary) :: {:ok, Kvasir.Event.t()} | {:error, term}
-          def bin_decode(event)
-          unquote(bin_decode)
-          def bin_decode(_), do: {:error, :unknown_event_type}
+           ```elixir
+           iex> bin_decode(<<...>>)
+           {:ok, ⊰Event{...}⊱}
+           ```
+           """
+           @spec bin_decode(binary) :: {:ok, Kvasir.Event.t()} | {:error, term}
+           def bin_decode(event)
+           unquote(bin_decode)
+           def bin_decode(_), do: {:error, :unknown_event_type}
 
-          defp event(type)
-          unquote(event)
-          defp event(_), do: {:error, :unknown_event_type}
+           defp event(type)
+           unquote(event)
+           defp event(_), do: {:error, :unknown_event_type}
 
-          defp bin_decode(event, key, version, data) do
-            with {:ok, decrypted} <- decrypt(event, data),
-                 {:ok, decompressed} <- decompress(event, decrypted) do
-              binary_decode(event, key, version, decompressed)
-            end
-          end
+           defp bin_decode(event, key, version, data) do
+             with {:ok, decrypted} <- decrypt(event, data),
+                  {:ok, decompressed} <- decompress(event, decrypted) do
+               binary_decode(event, key, version, decompressed)
+             end
+           end
 
-          defp decompress(event, data)
-          unquote(decompression)
+           defp decompress(event, data)
+           unquote(decompression)
 
-          defp decrypt(event, data)
-          unquote(decryption)
+           defp decrypt(event, data)
+           unquote(decryption)
 
-          unquote(opts[:extra])
-        end
-      end
+           unquote(opts[:extra])
+         end
+       end}
     end
   end
 
