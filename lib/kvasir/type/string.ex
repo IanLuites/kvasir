74f442d31e297eec15ae02cc8ca4ba8be3fa6671
Ian Luites <IanLuites@users.noreply.github.com>
2020-04-16 08:19:34 +0200

    String length validation.

--- lib/kvasir/type/string.ex
+++ lib/kvasir/type/string.ex
@@ -6,7 +6,31 @@ defmodule Kvasir.Type.String do
 
   @impl Kvasir.Type
   def parse(value, opts \\ [])
-  def parse(value, _opts) when is_binary(value), do: {:ok, value}
+
+  def parse(value, opts) when is_binary(value) do
+    value = if(opts[:trim], do: String.trim(value), else: value)
+
+    case {opts[:min], opts[:max]} do
+      {nil, nil} ->
+        {:ok, value}
+
+      {min, nil} ->
+        if(String.length(value) < min, do: {:error, :string_too_short}, else: {:ok, value})
+
+      {nil, max} ->
+        if(String.length(value) > max, do: {:error, :string_too_long}, else: {:ok, value})
+
+      {min, max} ->
+        length = String.length(value)
+
+        cond do
+          length < min -> {:error, :string_too_short}
+          length > max -> {:error, :string_too_long}
+          :ok -> {:ok, value}
+        end
+    end
+  end
+
   def parse(_value, _opts), do: {:error, :invalid_string}
 
   @impl Kvasir.Type
